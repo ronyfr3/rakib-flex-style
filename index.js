0f445f8240a4801633eb37d4ffb170a251a31b0c
ronyfr3 <babamarony@gmail.com>
2021-09-18 21:10:37 +0600

    rakib-flex-style

---
+++ index.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const FlexStyle = styled.div`
+  display: flex;
+  flex-direction: ${(props) => (props.row ? "row" : "column")};
+  align-items: ${(props) =>
+    props.center ? "center" : props.end ? "flex-end" : "flex-start"};
+  justify-content: ${(props) =>
+    props.between
+      ? "space-between"
+      : props.around
+      ? "space-around"
+      : props.evenly
+      ? "space-evenly"
+      : "center"};
+  flex-wrap: ${(props) => (props.wrap ? "wrap" : "nowrap")};
+`;
+export default FlexStyle;
